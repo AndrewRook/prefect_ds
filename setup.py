d798e9d2931a51511e7f955060b96d4a7b7cdf9f
Andrew Schechtman-Rook <rook166@gmail.com>
2020-01-15 22:03:18 -0500

    initial setup.py created

---
+++ setup.py
@@ -0,0 +1,48 @@
+import os
+import re
+import sys
+
+from setuptools import setup, find_packages
+
+sys.path.append("prefect_ds")
+from prefect_ds import __version__
+
+
+CLASSIFIERS = [
+    "Development Status :: 4 - Beta",
+    "Natural Language :: English",
+    "License :: OSI Approved :: Apache Software License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3 :: Only"
+]
+INSTALL_REQUIRES = [
+    'prefect >= 0.9.0, <= 0.9.0',
+    'pandas >= 0.25.3, <= 0.25.3'
+]
+
+EXTRAS_REQUIRE = {
+    "dev": ["pytest >= 5.3.2, <= 5.3.2", "pytest-cov >= 2.8.1, <= 2.8.1"]
+    }
+
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(HERE, 'README.md'),'r') as f:
+    README = f.read()
+
+###################################################################
+
+if __name__ == "__main__":
+    setup(
+        name="prefect_ds",
+        description='Tools for making Prefect work better for typical data science workflows',
+        license='Apache License 2.0',
+        url='https://github.com/AndrewRook/prefect_ds',
+        version=__version__,
+        author='Andrew Schechtman-Rook',
+        maintainer='Andrew Schechtman-Rook',
+        long_description=README,
+        packages=find_packages(where="."),
+        classifiers=CLASSIFIERS,
+        install_requires=INSTALL_REQUIRES,
+        extras_require=EXTRAS_REQUIRE
+    )
