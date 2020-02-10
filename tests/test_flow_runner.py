69bb1564910edeb2905c4dc792bc6d04b50df840
Andrew Schechtman-Rook <rook166@gmail.com>
2020-02-09 21:45:01 -0500

    most of way through flowrunner tests

--- tests/test_flow_runner.py
+++ tests/test_flow_runner.py
@@ -1,8 +1,12 @@
 import pandas as pd
+import pathlib
 import pytest
 from prefect import Flow, Parameter, task
 
 from prefect_ds.flow_runner import DSFlowRunner
+from prefect_ds.result import PurgedResult
+
+THIS_DIR = pathlib.Path(__file__).parent.absolute()
 
 @task()
 def create_offsets(num_offsets):
@@ -14,6 +18,7 @@ def create_data(offset):
         "one": [1, 2, 3],
         "two": [4, 5, 6]
     })
+    return data
 
 @task()
 def merge_data(data_list):
@@ -21,14 +26,14 @@ def merge_data(data_list):
 
 @task()
 def merge_two_dataframes(dataframe_1, dataframe_2):
-    return pd.concat([dataframe_1, dataframe_2])
+    return pd.concat([dataframe_1, dataframe_2], ignore_index=True)
 
 @task()
 def modify_data(input_data):
     return input_data * 2
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="module")
 def test_flow():
     with Flow("test") as flow:
         num_offsets = Parameter("num_offsets")
@@ -41,8 +46,57 @@ def test_flow():
         fully_combined_data = merge_two_dataframes(merged_data, merged_data_2)
         combined_modified_data = merge_two_dataframes(merged_data, modified_merged_data)
 
-    return DSFlowRunner(flow=flow)
+    tasks = {
+        "num_offsets": num_offsets,
+        "offsets": offsets,
+        "initial_data_list": initial_data_list,
+        "initial_data_list_2": initial_data_list_2,
+        "merged_data": merged_data,
+        "merged_data_2": merged_data_2,
+        "modified_merged_data": modified_merged_data,
+        "fully_combined_data": fully_combined_data,
+        "combined_modified_data": combined_modified_data
+    }
+
+    return DSFlowRunner(flow=flow), tasks
+
 
 def test_runs_at_all(test_flow):
-    #breakpoint()
-    test_flow.run(parameters={"num_offsets": 2}, return_tasks=test_flow.flow.tasks)
+    flow, tasks = test_flow
+    state = flow.run(parameters={"num_offsets": 2}, return_tasks=flow.flow.tasks)
+    print(state.result)
+
+
+def test_gets_expected_answers(test_flow):
+    flow, tasks = test_flow
+    state = flow.run(
+        parameters={"num_offsets": 2},
+        return_tasks=[tasks["fully_combined_data"], tasks["combined_modified_data"]]
+    )
+
+    expected_fully_combined_data = pd.DataFrame({
+        "one": [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3],
+        "two": [4, 5, 6, 4, 5, 6, 4, 5, 6, 4, 5, 6]
+    })
+    pd.testing.assert_frame_equal(
+        expected_fully_combined_data,
+        state.result[tasks["fully_combined_data"]].result
+    )
+
+    expected_combined_modified_data = pd.DataFrame({
+        "one": [1, 2, 3, 1, 2, 3, 2, 4, 6, 2, 4, 6],
+        "two": [4, 5, 6, 4, 5, 6, 8, 10, 12, 8, 10, 12]
+    })
+    pd.testing.assert_frame_equal(
+        expected_combined_modified_data,
+        state.result[tasks["combined_modified_data"]].result
+    )
+
+
+def test_purges_upstream_tasks(test_flow):
+    flow, tasks = test_flow
+    state = flow.run(parameters={"num_offsets": 2}, return_tasks=flow.flow.tasks)
+    for task_name, task_object in tasks.items():
+        if task_name not in ("fully_combined_data", "combined_modified_data"):
+            #breakpoint()
+            assert state.result[task_object]._result is PurgedResult
